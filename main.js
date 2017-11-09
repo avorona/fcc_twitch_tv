5c1305c87009ddda054a649e9c616016a8c51921
AVorona <vitalii.parshikov@gmail.com>
2017-11-10 00:05:34 +0200

    all streams+ channels:online+offline

--- main.js
+++ main.js
@@ -1,66 +1,75 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-	var outputWrapper=document.querySelector(".js-twitch-output"),
-			triggers=document.querySelectorAll(".js-filter-status");
-
-console.log(triggers);
-
-
-	var requestData = {
-     clientID : "l474jl9t8k6vb1cyvtmlljrn90i7r7",
-     clientSecret : "1otcv927vx32p06bse3mznppnbi4nj",
-     queries : [
-        "ESL_SC2",
-        "OgamingSC2",
-        "cretetion",
-        "freecodecamp",
-        "storbeck",
-        "habathcx",
-        "RobotCaleb",
-        "noobs2ninjas"
-    ],
-     API:  "https://api.twitch.tv/kraken/search/streams"
-
-	}
-
-
-
-var channelsData=new Array;
-
-requestData.queries.forEach(function (el) {
-
-return	sendRequest(el,channelsData,requestData);
-
-});
-
-// console.log(channelsData);
+    var outputWrapper = document.querySelector(".js-twitch-output"),
+        triggers = document.querySelectorAll(".js-filter-status");
 
 
 
 
+    var requestData = {
+        clientID: "l474jl9t8k6vb1cyvtmlljrn90i7r7",
+        clientSecret: "1otcv927vx32p06bse3mznppnbi4nj",
+        queries: [
+            "ESL_SC2",
+            "OgamingSC2",
+            "cretetion",
+            "freecodecamp",
+            "storbeck",
+            "geekandsundry",
+            "RobotCaleb",
+            "noobs2ninjas"
+        ],
+        API: "https://api.twitch.tv/kraken/search/streams",
+        APIOff: "https://api.twitch.tv/kraken/search/channels"
 
-triggers.forEach(function(el) {
+    }
 
-el.addEventListener('click', function(event) {
 
-console.log(event.currentTarget.getAttribute('data-filter-status'));
 
-switch (event.currentTarget.getAttribute('data-filter-status')) {
+    var channelsData = new Array;
 
-		case 'online':
-		showOnline(outputWrapper,channelsData);
-		break;
-		case 'offline':
-		showOffline(outputWrapper,channelsData);
-		break;
-	default:
-		showAll(outputWrapper,channelsData);
-		break;
-}
+    requestData.queries.forEach(function(el) {
 
-})
+        return sendRequest(el, channelsData, requestData, true);
 
-});
+    });
+
+
+    var streamsData = new Array;
+
+    requestData.queries.forEach(function(el) {
+
+        return sendRequest(el, streamsData, requestData);
+
+    });
+    // console.log(channelsData);
+
+
+
+
+
+    triggers.forEach(function(el) {
+
+        el.addEventListener('click', function(event) {
+
+            // console.log(event.currentTarget.getAttribute('data-filter-status'));
+
+            switch (event.currentTarget.getAttribute('data-filter-status')) {
+
+                case 'online':
+                    filterOnline(outputWrapper, streamsData);
+                    break;
+                case 'offline':
+                    filterOffline(outputWrapper, channelsData);
+                    break;
+                default:
+                    filterAll(outputWrapper, channelsData);
+                    break;
+            }
+
+        })
+
+    });
 
 
 
@@ -75,10 +84,17 @@ switch (event.currentTarget.getAttribute('data-filter-status')) {
 // send rquest
 
 
-function sendRequest(streamChannel, responseArray, requestParams) {
-		var  arrayOfResponses=responseArray;
-    var url = requestParams.API + "/?query=" + streamChannel + "&client_id=" + requestParams.clientID +
-        "&client_secret" + requestParams.clientSecret + "?callback=?";
+function sendRequest(streamChannel, responseArray, requestParams, offline) {
+    var arrayOfResponses = responseArray;
+
+    if (offline === true) {
+        var url = requestParams.APIOff + "/?query=" + streamChannel + "&client_id=" + requestParams.clientID +
+            "&client_secret=" + requestParams.clientSecret + "?callback=?";
+    } else {
+        var url = requestParams.API + "/?query=" + streamChannel + "&client_id=" + requestParams.clientID +
+            "&client_secret=" + requestParams.clientSecret + "?callback=?";
+    }
+
 
     function createCORSRequest(method, url) {
         var xhr = new XMLHttpRequest();
@@ -113,7 +129,7 @@ function sendRequest(streamChannel, responseArray, requestParams) {
         var text = xhr.responseText;
         var parsedJSON = JSON.parse(text);
         // console.log(parsedJSON);
-        fillResponseArray(parsedJSON,arrayOfResponses);
+        fillResponseArray(parsedJSON, arrayOfResponses);
 
     };
 
@@ -126,25 +142,193 @@ function sendRequest(streamChannel, responseArray, requestParams) {
 }
 
 
-function fillResponseArray(object,array) {
+function fillResponseArray(object, array) {
 
-let el=object;
+    let el = object;
 
-array.push(el);
+    array.push(el);
 
-return array;
+    return array;
 }
 
 
-function 	showOnline(output,data) {
+function filterAll(output, data) {
+
+let streamChannel = [];
+
+    data.forEach(function(el) {
+
+        el.channels.forEach(function(element, index) {
+
+                streamChannel.push(element);
+
+        });
 
 
-let outputHTML='<div class="output-wrapper__item"><div class="stream">'+
-	'<a href="" class="stream__link">'+
-	'<div class="stream__img" style="background-image: ; background-size: cover"></div>'+
-	'<h3 class="stream__heading"></h3></a></div></div>';
+    });
+
+    HTMLToDOM(output, streamChannel, "all");
 
 
-output.html=outputHTML;
+
 
 }
+
+
+function filterOnline(output, data) {
+
+
+    let streamChannel = [];
+
+
+    data.forEach(function(el) {
+
+        if (el.streams.length !== 0) {
+
+            streamChannel.push(el);
+
+            HTMLToDOM(output, streamChannel, 'online');
+
+        }
+
+
+    });
+
+
+}
+
+
+function filterOffline(output, data) {
+
+    let streamChannel = [];
+
+    data.forEach(function(el) {
+
+        el.channels.forEach(function(element, index) {
+
+
+            if (element.status === "") {
+
+                // let streamChannel=element;
+
+                streamChannel.push(element);
+
+            }
+
+        });
+
+
+    });
+
+    HTMLToDOM(output, streamChannel, "offline");
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function HTMLToDOM(output, stream, status) {
+
+
+    output.innerHTML = " ";
+
+
+    console.log(stream);
+    // console.log(stream);
+
+    if (status === "online") {
+
+
+
+        stream.forEach(function(el) {
+
+            let outputBox = document.createElement('div');
+            outputBox.className += ('output-wrapper__item');
+
+
+            outputBox.innerHTML = '<div class="stream">' +
+                '<a href="' + el.streams[0].channel.url + '" class="stream__link">' +
+                '<div class="stream__left"> <div class="stream__img" style="background-image: url(' + el.streams[0].channel.logo + '); background-size: cover"></div></div>' +
+                '<div class="stream__right"><h3 class="stream__heading">' + el.streams[0].game + '</h3>' +
+                '<p class="stream__descr">' + el.streams[0].channel.status + '</p></div></a></div>';
+
+
+
+            output.append(outputBox);
+
+
+
+        });
+
+    } else if (status === "offline") {
+
+        stream.forEach(function(el) {
+
+                let outputBox = document.createElement('div');
+                outputBox.className += ('output-wrapper__item');
+
+
+                outputBox.innerHTML = '<div class="stream stream_offline">' +
+                    '<a href="' + el.url + '" class="stream__link">' +
+                    '<div class="stream__left"> <div class="stream__img" style="background-image: url(http://fillmurray.com/150/150); background-size: cover"></div></div>' +
+                    '<div class="stream__right"><h3 class="stream__heading">' + el.display_name + '</h3>' +
+                    '</div></a></div>';
+
+
+
+                output.append(outputBox);
+
+
+            });
+
+        } else if (status==="all") {
+
+        	      stream.forEach(function(el) {
+
+        	      	let statusClass="";
+
+        	      	if(el.status==="") {
+        	      		statusClass="offline"
+
+        	      	} else {
+        	      		statusClass="online"
+        	      	}
+
+                let outputBox = document.createElement('div');
+                outputBox.className += ('output-wrapper__item');
+
+
+                outputBox.innerHTML = '<div class="stream stream_'+statusClass+'">' +
+                    '<a href="' + el.url + '" class="stream__link">' +
+                    '<div class="stream__left"> <div class="stream__img" style="background-image: url(http://fillmurray.com/150/150); background-size: cover"></div></div>' +
+                    '<div class="stream__right"><h3 class="stream__heading">' + el.display_name + '</h3>' +
+                    '</div></a></div>';
+
+
+
+                output.append(outputBox);
+
+
+            });
+        }
+         else  {
+
+            alert('Give me some stuff, hey!');
+        }
+        
+
+
+
+
+
+    }
